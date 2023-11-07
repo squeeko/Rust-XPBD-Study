4bece1b83d2412ce13be0c5cc6319ffccaa7617f
squeeko <squeeko@users.noreply.github.com>
2023-11-07 14:42:46 -0800

    I love XPBD

---
+++ xpbd-threed/src/main.rs
@@ -0,0 +1,55 @@
+use bevy::prelude::*;
+use bevy_xpbd_3d::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins((DefaultPlugins, PhysicsPlugins::default()))
+        .add_systems(Startup, setup)
+        .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    //Plane
+    commands.spawn((
+        RigidBody::Static,
+        Collider::cuboid(8., 0.002, 8.),
+        PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Plane::from_size(8.))),
+            material: materials.add(Color::rgb(0.3, 0.5, 0.3).into()),
+            ..default()
+        },
+    ));
+
+    //Cube
+    commands.spawn((
+        RigidBody::Dynamic,
+        AngularVelocity(Vec3::new(2.5, 3.4, 1.6)),
+        Collider::cuboid(1., 1., 1.),
+        PbrBundle {
+            mesh: meshes.add(Mesh::from(shape::Cube { size: 1.0 })),
+            material: materials.add(Color::rgb(0.8, 0.7, 0.6).into()),
+            transform: Transform::from_xyz(0., 4., 0.),
+            ..default()
+        },
+    ));
+
+    // Light
+    commands.spawn(PointLightBundle {
+        point_light: PointLight {
+            intensity: 1500.,
+            shadows_enabled: true,
+            ..default()
+        },
+        transform: Transform::from_xyz(4., 8., 4.),
+        ..default()
+    });
+    // Camera
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(-4.0, 6.5, 8.0).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
+}
